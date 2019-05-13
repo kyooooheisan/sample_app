0498505ea3f9232a08d93de6ea2efd94b7a0b191
kyoheikokai <k.kyohei0915@gmail.com>
2019-05-13 14:24:31 +0000

    finish ch14

--- config/routes.rb
+++ config/routes.rb
@@ -13,13 +13,18 @@ Rails.application.routes.draw do
   get  '/login',   to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only:[:edit]
   resources :password_resets, only:[:new,:create,:edit,:update]
   resources :microposts, only:[:create,:destroy]
    #GET /account_activations/:id/edit
    #params[:id] <=== 有効化トークン
 # => contact_path
+  resources :relationships,       only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
